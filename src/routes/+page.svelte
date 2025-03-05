54239d36ce176b49cf8f3d0c45ad3135c649c123
mirfan777 <fcmirfan122@gmail.com>
2025-03-05 21:14:14 +0700

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Created by Maulana Irfan | Fahri ferdinnansya | Rio Radiansyah</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let count = 0;
+</script>
+
+<button on:click={() => count -= 1}>-</button>
+<span>{ count }</span>
+<button on:click={() => count += 1}>+</button>
