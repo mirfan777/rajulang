c82347ace61ff0bf91a4050a2f0044a18a29cb50
mirfan777 <fcmirfan122@gmail.com>
2025-03-05 21:16:57 +0700

    test2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <h1>Created by Maulana Irfan | Fahri ferdinnansya | Rio Radiansyah</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+Test
 <script>
 	let count = 0;
 </script>
